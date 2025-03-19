edbd2e0cf06408daf067d82d20cc8b5b19cc7c7a
Jande Masmo <jandemasmo@yahoo.com>
2025-03-18 22:25:59 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/com/jandemasmo/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/jandemasmo/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package com.jandemasmo.CadastroDeNinjas.Missoes;
 
 import com.jandemasmo.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/com/jandemasmo/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/jandemasmo/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,12 +2,16 @@ package com.jandemasmo.CadastroDeNinjas.Ninjas;
 
 import com.jandemasmo.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name ="tb_cadastro_de_ninjas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -27,36 +31,4 @@ public class NinjaModel {
 
 
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
